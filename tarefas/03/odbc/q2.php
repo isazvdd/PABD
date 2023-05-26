bbfbb72d35b9fcacf60521f3667c36fc6b57160f
isazvdd <isadora.azevedo.700@ufrn.edu.br>
2023-05-26 14:35:52 -0300

    feat: inserindo a questão 2 (#3)

---
+++ tarefas/03/odbc/q2.php
@@ -0,0 +1,24 @@
+<?php
+
+include('connection.php');
+
+$projetoCodigo = 2;
+
+$query = "SELECT a.codigo, a.descricao, a.dataInicio, a.dataFim, a.situacao, a.dataConclusao
+          FROM atividade a
+          INNER JOIN atividade_projeto ap ON a.codigo = ap.codAtividade
+          WHERE ap.codProjeto = ?";
+
+$statement = odbc_prepare($connection, $query);
+
+$result = odbc_execute($statement, array($projetoCodigo));
+
+if ($result) {
+  while ($row = odbc_fetch_array($statement)) {
+    echo "{$row['descricao']}\n";
+  }
+} else {
+  echo "Erro na consulta: " . odbc_errormsg($connection);
+}
+
+odbc_close($connection);
